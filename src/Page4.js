c5882e511dee7af5f787128b792f036be4515aaf
Kevin Nedelec <kevin.nedelec@gitguardian.com>
2020-07-09 10:13:04 +0200

    feat(page4): new page, new occurrence of the leak

---
+++ src/Page4.js
@@ -0,0 +1,12 @@
+
+export const MyKeyComponent = () => {
+  const key = '02Bc6972af6fF4e8eeA1cAabaad2532FdC3387b3e8aEfA2fc0e47b74abCeAf2A6F5f02f'
+  const sshKey = `
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClKsfkNkuSevGj3eYhCe53pcjqP3maAhDFcvBS7O6V
+hz2ItxCih+PnDSUaw+WNQn/mZphTk/a/gU8jEzoOWbkM4yxyb/wB96xbiFveSFJuOp/d6RJhJOI0iBXr
+lsLnBItntckiJ7FbtxJMXLvvwJryDUilBMTjYtwB+QhYXUMOzce5Pjz5/i8SeJtjnV3iAoG/cQk+0FzZ
+qaeJAAHco+CY/5WrUBkrHmFJr6HcXkvJdWPkYQS3xqC0+FmUZofz221CBt5IMucxXPkX4rWi+z7wB3Rb
+BQoQzd8v7yeb7OzlPnWOyN0qFU0XA246RA8QFYiCNYwI3f05p6KLxEXAMPLE`
+
+  return null
+}
